480c968002ec49f2feda525d2e0bf2d797d19be6
ravikumar.venkatesan <ravikumar.venkatesan@syncfusion.com>
2023-03-21 20:25:24 +0530

    Syncfusion Blazor Scheduler with CustomAdaptor project updated.

---
+++ Data/AppointmentDataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace syncfusion_blazor_app.Data
+{
+    public class AppointmentDataContext: DbContext
+    {
+        public AppointmentDataContext(DbContextOptions<AppointmentDataContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppointmentData>().HasData(
+                new AppointmentData
+                {
+                    RecordID = 1,
+                    Id = 1,
+                    Subject = "Meeting",
+                    StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 10, 0, 0),
+                    EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 12, 0, 0),
+                    Location = "Tamilnadu"
+                }
+            ); ;
+        }
+        public DbSet<AppointmentData> AppointmentDatas { get; set; }
+    }
+}
